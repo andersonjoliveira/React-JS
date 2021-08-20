fff400ba977dc7f5608ccdd831c6ebf5b40d1de0
Anderson Oliveira <joseanderson33@gmail.com>
2021-08-19 22:53:01 -0300

    Update
    Curso React-JS
    
    Trabalhando com states
    Ciclo de vida dos componentes
    Eventos

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Membro from './components/Membro';
 
 class App extends Component{
+
     render(){
         return(
             <div>
-                
+                <Membro nome="Visitante"/>
             </div>
         );
+            
     }
 }
 
@@ -134,6 +137,99 @@ function App(){
     )
 }
 
+
+export default App;
+*/
+
+/*
+
+35. Trabalhando com states
+
+import React, { Component } from 'react';
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            contador: 0
+        };
+
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+        let state = this.state;
+        state.contador +=1;
+        this.setState(state)
+    }
+
+    diminuir(){
+        let state = this.state;
+        if(state.contador === 0){
+            alert('Opa chegou a zero!');
+            return;
+        }
+
+        state.contador -=1;
+        this.setState(state)
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Contador</h1>
+                
+                <h3>
+                    <button onClick={this.diminuir}>-</button> 
+                    {this.state.contador}
+                    <button onClick={this.aumentar}>+</button> 
+                </h3>    
+            </div>
+        );
+    }
+}
+
+export default App;
+
+*/
+
+/*
+37. Ciclo de vida dos componentes
+
+import React, { Component } from 'react';
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            hora: '00:00:00'
+         };
+    }
+    
+    componentDidMount(){
+
+        setInterval(()=>{
+            this.setState({ hora: new Date().toLocaleTimeString() })
+        }, 1000);
+
+    }
+    
+    componentDidUpdate(){
+        console.log('Atualizou!!!');
+    }
+
+        
+    render(){
+        return(
+            <div>
+                <h1>Meu Projeto {this.state.hora}</h1>
+            </div>
+        );
+    }
+}
 
 export default App;
 */
